74f7238c72d8f54dcf5c56971c9582fc3afdb495
twinprice <twinprice@hotmail.com>
2025-02-13 12:34:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const SHARED_PASSWORD = "secure123";
-const DATA_URL = "https://script.google.com/macros/s/AKfycbx0nkRxnufaD8CDEFYLQlmaDdGrkPBsKLCtZ5X6U85f5bXHePPJgEcxqfcP4_rl5bpE/exec"; // Replace with your actual Apps Script URL
+const DATA_URL = "https://script.google.com/macros/s/AKfycbxFn_h-tezcMaf0HgtHLWYeG_A-kc5Pdb6oeuVO6tOxUd4bnsC6GrRiMvGYr8UfM1qE/exec"; // Replace with your actual Apps Script URL
 const CAMP_BADGES = [
   "Amer. Heritage", "Animation", "Archery", "Art", "Astronomy", "Basketry", "Camping", 
   "Canoeing", "Chemistry", "Chess", "Cit. in Comm.", "Cit. in Nation", "Cit. in World", 
